e76f41e6826973d2c3140840ad31cec75d4e9021
Breina <info@breina.be>
2022-07-28 17:19:55 +0200

    Like this? No idea

--- custom_components/powertag_gateway/soap_communication.py
+++ custom_components/powertag_gateway/soap_communication.py
@@ -31,4 +31,4 @@ async def transfer_get(service: Service, address: (str | bytes), hass=HomeAssist
         .replace("{{MessageID}}", str(message_id)) \
         .replace("{{OurID}}", str(our_id))
 
-    return await hass.async_add_executor_job(requests.post(address, data=get_device))
+    return await hass.async_add_executor_job(requests.post, address, data=get_device)
